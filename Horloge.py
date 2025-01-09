27a5fbfe7d277d068bde3f71381ceb6fdc76b778
Axel Ripamonti-Cabrera <axel.ripamonti-cabrera@laplateforme.io>
2025-01-09 12:27:08 +0100

    Update Horloge.py

--- Horloge.py
+++ Horloge.py
@@ -2,30 +2,35 @@ from datetime import datetime
 import time
 
 current = None
-mode = int(input("1. Heure actuelle\n2. Heure spécifiée :\n"))
+
 
 def current_time():
     current = datetime.now().strftime("%H:%M:%S")
     print(current, end = "\r")
 
-def set_time(current):
-    current = datetime(int(input("Donner une heure"))).strftime("%H:%M:%S")
-    print(current, end = "\r")
+def set_time():
+    current = input("Donner une heure (format HH:MM:SS) : ")
+    try:
+        current = datetime.strptime(current, "%H:%M:%S").strftime("%H:%M:%S")
+    except ValueError:
+        print("Format invalide. Veuillez entrer l'heure au format HH:MM:SS.")
 
 def counting():
+    mode = int(input("1. Heure actuelle\n2. Heure spécifiée :\n"))
     if mode == 1 : 
         while True :
             current_time()
             time.sleep(1)
     elif mode == 2 :
+        set_time()
         while True :
-            set_time()
+            print(current)
             time.sleep(1)
     else :
         print("Mauvaise entrée")
 
 
-
+counting()
     
 
 
