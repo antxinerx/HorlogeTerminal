34d4fd61e249a08adedf8293ecdb7d70eedc7178
Axel Ripamonti-Cabrera <axel.ripamonti-cabrera@laplateforme.io>
2025-01-09 11:14:57 +0100

    Update Horloge.py

--- Horloge.py
+++ Horloge.py
@@ -1,13 +1,31 @@
 import datetime
 import time
 
+current = None
+
 def current_time():
     current = datetime.datetime.now().strftime("%H:%M:%S")
     print(current, end = "\r")
 
+#def set_time(current):
+
 def counting():
-    while True :
-        display_time()
-        time.sleep(1)
+    mode = int(input("1. Heure actuelle\n2. Heure spécifiée :\n "))
+    if mode == 1 : 
+        while True :
+            current_time()
+            time.sleep(1)
+    elif mode == 2 :
+        while True :
+            set_time()
+            time.sleep(1)
+    else :
+        print("Mauvaise entrée")
 
 counting()
+
+    
+
+
+
+
