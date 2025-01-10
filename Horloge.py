cb849776dd5cf5c305782c5bf12e539eab3325cf
Axel Ripamonti-Cabrera <axel.ripamonti-cabrera@laplateforme.io>
2025-01-10 10:15:25 +0100

    Update Horloge.py

--- Horloge.py
+++ Horloge.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime ,  timedelta
 import time
 
 current = None
@@ -9,13 +9,21 @@ def current_time():
     current = datetime.now().strftime("%H:%M:%S")
     print(current, end = "\r")
 
-def set_time():
-    global current 
-    current = input("Donner une heure (format HH:MM:SS) : ")
+def set_time(): 
+    user_input = input("Donner une heure (format HH:MM:SS) : ")
     try:
-        current = datetime.strptime(current, "%H:%M:%S").strftime("%H:%M:%S")
+        custom_time = datetime.strptime(user_input, "%H:%M:%S")
+        return custom_time
     except ValueError:
         print("Format invalide. Veuillez entrer l'heure au format HH:MM:SS.")
+
+    
+def set_alarm_and_format():
+    set_alarm
+    yes_or_no = input("Configurer une alarme ? o/n")
+    if yes_or_no == "o":
+        alarm = input("Donner une heure (format HH:MM:SS) : ")
+
 
 def counting():
     mode = int(input("1. Heure actuelle\n2. Heure spécifiée :\n"))
@@ -24,11 +32,10 @@ def counting():
             current_time()
             time.sleep(1)
     elif mode == 2 :
-         while current is None:  
-            set_time()
-         while True :
-            if current is not None:
-                print(current, end = "\r")
+        custom_time = set_time()
+        while True :
+                custom_time += timedelta(seconds= 1)
+                print(custom_time.strftime("%H:%M:%S"), end = "\r")
                 time.sleep(1)
     else :
         print("Mauvaise entrée")
